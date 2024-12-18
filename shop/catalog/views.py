b4b0548b2956c23d78338d2e31a2eb5aa9964495
nsazhi <n190275@inbox.ru>
2024-12-18 19:34:55 +0400

    18/12/24 Add DTL

--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -4,18 +4,17 @@ from django.views import View
 
 # Create your views here.
 def index(request):
-    return render(request, 'catalog/index.html')
+    # products = []
+    products = [
+        {"name": "Смартфон", "price": 15000},
+        {"name": "Ноутбук", "price": 55000},
+        {"name": "Планшет", "price": 25000},
+    ]
+    return render(request, 'catalog/index.html', {'products': products})
 
 class IndexView(View):
     def get(self, request):
         return HttpResponse("Добро пожаловать в интернет-магазин!")
-
-
-products = [
-    {"name": "Смартфон", "price": 15000},
-    {"name": "Ноутбук", "price": 55000},
-    {"name": "Планшет", "price": 25000},
-]
 
 def product_list(request):
     product_items = "<br>".join([f"{p['name']}: {p['price']} руб." for p in products])
