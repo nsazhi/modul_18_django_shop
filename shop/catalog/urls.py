4cfe0e00f0c5177ea7e7f8fec931f664eae836fb
nsazhi <n190275@inbox.ru>
2024-12-20 17:41:34 +0400

    20/12/24 Add GET, POST, form

--- shop/catalog/urls.py
+++ shop/catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import IndexView, ProductListView, product_list, index
+from .views import product_list, add_product # index, IndexView, ProductListView
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
     # path('', IndexView.as_view(), name='index'), # Метод as_view() подключает класс
-    path('products/', product_list, name='product_list'), # Функциональное представление
-    path('products-class/', ProductListView.as_view(), name='product_list_class'), # Классовое представление
+    # path('products/', product_list, name='product_list'), # Функциональное представление
+    # path('products-class/', ProductListView.as_view(), name='product_list_class'), # Классовое представление
+    path('', product_list, name='product_list'),
+    path('add/', add_product, name='add_product'),
 ]
--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 
 # Create your views here.
+products = [
+    {'id': 1, 'name': 'Смартфон Samsung Galaxy', 'category': 'телефоны', 'price': 15000},
+    {'id': 2, 'name': 'Планшет Apple iPad', 'category': 'планшеты', 'price': 45000},
+    {'id': 3, 'name': 'Наушники Sony WH-1000XM4', 'category': 'аксессуары', 'price': 25000},
+    {'id': 4, 'name': 'Ноутбук Dell XPS 13', 'category': 'ноутбуки', 'price': 95000},
+    {'id': 5, 'name': 'Смарт-часы Xiaomi Mi Band', 'category': 'аксессуары', 'price': 3000},
+]
+
+
 def index(request):
     # products = []
     products = [
@@ -12,15 +21,54 @@ def index(request):
     ]
     return render(request, 'catalog/index.html', {'products': products})
 
-class IndexView(View):
-    def get(self, request):
-        return HttpResponse("Добро пожаловать в интернет-магазин!")
+
+# class IndexView(View):
+#     def get(self, request):
+#         return HttpResponse("Добро пожаловать в интернет-магазин!")
+
+
+# def product_list(request):
+#     product_items = "<br>".join([f"{p['name']}: {p['price']} руб." for p in products])
+#     return HttpResponse(product_items)
+
+
+# class ProductListView(View):
+#     def get(self, request):
+#         product_items = "<br>".join([f"{p['name']}: {p['price']} руб." for p in products])
+#         return HttpResponse(product_items)
+
 
 def product_list(request):
-    product_items = "<br>".join([f"{p['name']}: {p['price']} руб." for p in products])
-    return HttpResponse(product_items)
+    # Получаем параметр 'category' из строки запроса
+    category = request.GET.get('category')
 
-class ProductListView(View):
-    def get(self, request):
-        product_items = "<br>".join([f"{p['name']}: {p['price']} руб." for p in products])
-        return HttpResponse(product_items)
+    # Если категория указана, фильтруем товары
+    if category:
+        filtered_products = [product for product in products if product['category'] == category]
+    else:
+        filtered_products = products
+
+    # Передаем товары в шаблон
+    return render(request, 'catalog/product_list.html', {'products': filtered_products})
+
+
+def add_product(request):
+    if request.method == 'POST':
+        # Получаем данные из формы
+        name = request.POST.get('name')
+        category = request.POST.get('category')
+        price = request.POST.get('price')
+
+        # Добавляем новый товар в список
+        products.append({
+            'id': len(products) + 1,
+            'name': name,
+            'category': category,
+            'price': int(price),
+        })
+
+        # Перенаправляем на список товаров
+        return redirect('product_list')  # Имя маршрута списка товаров
+
+    # Если GET-запрос, отображаем форму
+    return render(request, 'catalog/add_product.html')
